262aad7d18bca10492db50125aacb6e50297bcc3
Fábio <fflucas@hotmail.com>
2021-04-23 14:05:51 -0300

    Endpoint para listar livros

--- src/main/java/br/com/zupacademy/fabio/casadocodigo/livro/ControladorLivro.java
+++ src/main/java/br/com/zupacademy/fabio/casadocodigo/livro/ControladorLivro.java
@@ -2,14 +2,13 @@ package br.com.zupacademy.fabio.casadocodigo.livro;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/livro")
@@ -17,17 +16,21 @@ public class ControladorLivro {
 
     @PersistenceContext
     private EntityManager entityManager;
-    private RepositorioLivro repositorioLivro;
     @Autowired
-    public ControladorLivro(EntityManager entityManager, RepositorioLivro repositorioLivro) {
+    public ControladorLivro(EntityManager entityManager) {
         this.entityManager = entityManager;
-        this.repositorioLivro = repositorioLivro;
     }
 
     @PostMapping
+    @Transactional
     public ResponseEntity<ModeloLivro> create(@RequestBody @Valid DtoLivro dtoLivro){
         ModeloLivro livro = dtoLivro.converteParaLivro(entityManager);
-        repositorioLivro.save(livro);
+        entityManager.persist(livro);
         return ResponseEntity.ok(livro);
+    }
+
+    @GetMapping
+    public List listAll(){
+        return entityManager.createNativeQuery("SELECT l.id, l.titulo FROM livro l").getResultList();
     }
 }
